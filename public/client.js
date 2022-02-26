1ee7d37902fabb2e2379c329e0871f411fd87b65
anshaggarwal1 <anshaggarwal1968@gmail.com>
2022-02-26 21:23:48 +0530

    chatApp

---
+++ public/client.js
@@ -0,0 +1,51 @@
+const socket = io()
+let name1 ;
+let textarea = document.querySelector('#text')
+let messageArea = document.querySelector('.msg')
+do {
+    name1 = prompt('Please enter your name1: ')
+} while(!name1)
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: name1,
+        message: message.trim()
+    }
+    // Append 
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+
+    // Send to server 
+    socket.emit('message', msg)
+
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let classname1 = type
+    mainDiv.classList.add(classname1, 'message')
+
+    let markup = `
+        <h5>${msg.user}</h5>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+}
+
+// Recieve messages 
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
